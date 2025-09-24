bfb631fcab865a7208e979c3826caaf3199ba940
Lou DE BAERE <lou_debaere@ens.univ-artois.fr>
2025-09-24 08:52:45 +0200

    commit initial, création du projet

---
+++ src/app/components/a-propos/a-propos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-a-propos',
+  imports: [],
+  templateUrl: './a-propos.html',
+  styleUrl: './a-propos.css'
+})
+export class APropos {
+
+}
