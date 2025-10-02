d6a5c9ed85ebde03dc75e4ee970fb5f0640e8c65
Lou DE BAERE <lou_debaere@ens.univ-artois.fr>
2025-10-02 20:19:02 +0200

    mise à jour du header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrls: ['./header.css']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  protected isMenuOpen = signal(false);
 
+  toggleMenu() {
+    this.isMenuOpen.update(value => !value);
+  }
+
+  closeMenu() {
+    this.isMenuOpen.set(false);
+  }
+}
