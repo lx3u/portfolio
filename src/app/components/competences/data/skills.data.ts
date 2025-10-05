491a1992e56066d2e02d21fa2c3fb83116852acb
lx3u <lou.debaere62@gmail.com>
2025-10-05 18:33:19 +0200

    ajout de compétences

--- src/app/components/competences/data/skills.data.ts
+++ src/app/components/competences/data/skills.data.ts
@@ -21,10 +21,30 @@ export const SKILLS_DATA: SkillCategory[] = [
     skills: [
       { name: 'Java', logo: '/assets/logos/java.svg' },
       { name: 'Python', logo: '/assets/logos/python.svg' },
+      { name: 'Bash', logo: '/assets/logos/bash.svg' },
     ],
   },
   {
     title: 'Base de données',
-    skills: [{ name: 'PostgreSQL', logo: '/assets/logos/postgresql.svg' }],
+    skills: [
+      { name: 'PostgreSQL', logo: '/assets/logos/postgresql.svg' },
+      { name: 'Conception MCD/MLD', logo: '/assets/logos/bdd.svg' },
+    ],
+  },
+  {
+    title: 'Versionning et travail collaboratif',
+    skills: [
+      { name: 'Git', logo: '/assets/logos/git.svg' },
+      { name: 'GitLab', logo: '/assets/logos/gitlab.svg' },
+      { name: 'GitHub', logo: '/assets/logos/github.svg' },
+    ],
+  },
+  {
+    title: 'IDEs',
+    skills: [
+      { name: 'Visual Studio Code', logo: '/assets/logos/vscode.svg' },
+      { name: 'Suite JetBrains', logo: '/assets/logos/jetbrains.svg' },
+      { name: 'Eclipse', logo: '/assets/logos/eclipse.svg' },
+    ],
   },
 ];
