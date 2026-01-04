ba2065ab3b4233d4800d4ddc69956b2f0f056c08
Lou De Baere <lou.debaere62@gmail.com>
2026-01-04 02:33:00 +0100

    ajout nouveau projet + amélioration page projet

--- src/app/components/projets/projets.ts
+++ src/app/components/projets/projets.ts
@@ -10,37 +10,41 @@ import { Component, OnInit } from '@angular/core';
 export class ProjetsComponent implements OnInit {
   projects = [
     {
-      id: 1,
-      name: 'PacMan (EN COURS)',
+      name: 'Blog de musique - O Sol Sound',
       description:
-        "Clone du jeu PacMan en Java/JavaFX (2ème année BUT Informatique), réalisé en groupe, pour continuer à approfondir l'apprentissage de Java et découvrir les patrons de conception.",
-      image: 'assets/img_projet3.png',
-      githubUrl: 'https://github.com/lx3u/pacman',
-      tags: ['Java', 'JavaFX'],
+        "Conception d'un site web dynamique réalisé en équipe lors d'un marathon de 36h, sur le thème de la Bossa Nova, réalisé avec le framework Laravel.",
+      image: 'assets/img_projet5.png',
+      githubUrl: 'https://github.com/lx3u/projet-marathon',
+      tags: ['Laravel', 'PHP', 'HTML', 'CSS'],
     },
     {
-      id: 2,
-      name: 'Portfolio web (EN COURS)',
+      name: 'Portfolio web',
       description:
-        'Portfolio personnel pour présenter mon parcours et mes compétences, réalisé avec le framework Angular (dans le but de découvrir de nouvelles manières de développer des sites web).',
+        'Portfolio personnel réalisé avec Angular pour présenter mes projets et compétences et pour prendre en main pour la première fois ce framework.',
       image: 'assets/img_projet2.png',
       githubUrl: 'https://github.com/lx3u/portfolio',
       tags: ['Angular', 'TypeScript', 'HTML', 'CSS'],
     },
     {
-      id: 3,
+      name: 'PacMan',
+      description:
+        "Clone du jeu PacMan en Java/JavaFX réalisé lors d'un projet scolaire afin d'approfondir mes compétences en programmation orientée objet et de découvrir les patrons de conception.",
+      image: 'assets/img_projet3.png',
+      githubUrl: 'https://github.com/lx3u/pacman',
+      tags: ['Java', 'JavaFX'],
+    },
+    {
       name: 'Bomberman',
       description:
-        "Clone du jeu Bomberman en Java et JavaFX (2ème semestre BUT Informatique) afin de découvrir le développement d'application avec IHM.",
+        "Clone du jeu Bomberman en Java/JavaFX réalisé lors d'un projet scolaire pour découvrir le découpage MVC.",
       image: 'assets/img_projet4.png',
       githubUrl: 'https://github.com/lx3u/bomberman',
       tags: ['Java', 'JavaFX'],
     },
     {
-      id: 4,
       name: "Site vitrine - L'Air du cinéma",
       description:
-        'Premier projet réalisé en binôme en cours (1er semestre BUT Informatique) pour mettre en pratique notre maîtrise de HTML/CSS.',
+        "Réalisation d'un site vitrine pour une entreprise d'événementiel fictive en HTML et CSS.",
       image: 'assets/img_projet1.png',
       githubUrl: 'https://github.com/Shun-codes/L-Air-du-Cinema',
       tags: ['HTML', 'CSS'],
